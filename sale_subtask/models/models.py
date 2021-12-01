dbe1bb18627bd28cfefab13e603eb97128fe7634
Gerardo Andres <gquispeh@uni.pe.com>
2021-12-01 04:13:16 -0500

    init

--- sale_subtask/models/models.py
+++ sale_subtask/models/models.py
@@ -12,7 +12,13 @@ class SaleOrderLineTask(models.Model):
     def button_task(self):
         print('button task')
 
+class SaleOrderLineTaskWizardTree(models.TransientModel):
+    _name = "sale.order.line.wizard.tree"
+    name = fields.Char(string='Tarea')
+    wizard_id = fields.Many2one('sale.order.line.wizard', string='Sale task')
+
 class SaleOrderLineTaskWizard(models.TransientModel):
     _name = "sale.order.line.wizard"
     
     sale_id = fields.Many2one('sale.order.line', string='Linea de Venta')
+    subtask_ids = fields.One2many('sale.order.line.subtask', 'line_id', string='Sub Tareas')
