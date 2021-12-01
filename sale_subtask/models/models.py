cc3273d368eaa549dbcfa75241aee199d34dd760
Gerardo Andres <gquispeh@uni.pe.com>
2021-12-01 04:41:35 -0500

    comentar one2many

--- sale_subtask/models/models.py
+++ sale_subtask/models/models.py
@@ -4,6 +4,10 @@ class SaleOrderLineSubtask(models.Model):
     _name = "sale.order.line.subtask"
     name = fields.Char(string='Tarea')
     line_id = fields.Many2one('sale.order.line',string="Linea")
+    state = fields.Selection([
+        ('0', 'borrador'),
+        ('1', 'activo')
+    ], string='Estado')
 
 class SaleOrderLineTask(models.Model):
     _name = "sale.order.line"
@@ -21,13 +25,21 @@ class SaleOrderLineTask(models.Model):
             'target': 'new',
         }
 
-class SaleOrderLineTaskWizardTree(models.TransientModel):
-    _name = "sale.order.line.wizard.tree"
-    name = fields.Char(string='Tarea')
-    wizard_id = fields.Many2one('sale.order.line.wizard', string='Tarea de linea de venta')
+#class SaleOrderLineTaskWizardTree(models.TransientModel):
+#    _name = "sale.order.line.wizard.tree"
+#    name = fields.Char(string='Tarea')
+#    wizard_id = fields.Many2one('sale.order.line.wizard', string='Tarea de linea de venta')
 
 class SaleOrderLineTaskWizard(models.TransientModel):
     _name = "sale.order.line.wizard"
     
-    sale_id = fields.Many2one('sale.order.line', string='Linea de Venta')
-    subtask_ids = fields.One2many('sale.order.line.wizard.tree', 'wizard_id', string='Sub Tareas')
+    line_id = fields.Many2one('sale.order.line', string='Linea de Venta')
+    #subtask_ids = fields.One2many('sale.order.line.subtask', 'wizard_id', string='Sub Tareas')
+
+    def added(self):
+        pass
+#        for line in self.subtask_ids:
+#            subtask = self.env['sale.order.line.subtask'].sudo().create({
+#				'name':line.name,
+#				'line_id':line.sale_id.id,
+#				})
