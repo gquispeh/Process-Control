eed29dd9ada4a44c16fb2e8626d61e754e089ee0
Gerardo Andres <gquispeh@uni.pe.com>
2021-12-01 04:21:35 -0500

    button

--- sale_subtask/models/models.py
+++ sale_subtask/models/models.py
@@ -10,15 +10,24 @@ class SaleOrderLineTask(models.Model):
     subtask_ids = fields.One2many('sale.order.line.subtask', 'line_id', string='Sub Tareas')
 
     def button_task(self):
-        print('button task')
+        return {
+            'name': 'Sale Subtask',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': 'sale_order_line_wizard_form',
+            'res_model': 'sale.order.line.wizard',
+            'domain': [],
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
 
 class SaleOrderLineTaskWizardTree(models.TransientModel):
     _name = "sale.order.line.wizard.tree"
     name = fields.Char(string='Tarea')
-    wizard_id = fields.Many2one('sale.order.line.wizard', string='Sale task')
+    wizard_id = fields.Many2one('sale.order.line.wizard', string='Tarea de linea de venta')
 
 class SaleOrderLineTaskWizard(models.TransientModel):
     _name = "sale.order.line.wizard"
     
     sale_id = fields.Many2one('sale.order.line', string='Linea de Venta')
-    subtask_ids = fields.One2many('sale.order.line.subtask', 'line_id', string='Sub Tareas')
+    subtask_ids = fields.One2many('sale.order.line.subtask', 'wizard_id', string='Sub Tareas')
