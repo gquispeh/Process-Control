102cadb960c0184929c73144de484c59d8180bb3
Gerardo Andres <gquispeh@uni.pe.com>
2021-12-01 04:49:22 -0500

    comentar

--- sale_subtask/models/models.py
+++ sale_subtask/models/models.py
@@ -14,30 +14,31 @@ class SaleOrderLineTask(models.Model):
     subtask_ids = fields.One2many('sale.order.line.subtask', 'line_id', string='Sub Tareas')
 
     def button_task(self):
-        return {
-            'name': 'Sale Subtask',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'view_id': 'sale_order_line_wizard_form',
-            'res_model': 'sale.order.line.wizard',
-            'domain': [],
-            'type': 'ir.actions.act_window',
-            'target': 'new',
-        }
+        pass
+        #return {
+        #    'name': 'Sale Subtask',
+        #    'view_type': 'form',
+        #    'view_mode': 'form',
+        #    'view_id': 'sale_order_line_wizard_form',
+        #    'res_model': 'sale.order.line.wizard',
+        #    'domain': [],
+        #    'type': 'ir.actions.act_window',
+        #    'target': 'new',
+        #}
 
 #class SaleOrderLineTaskWizardTree(models.TransientModel):
 #    _name = "sale.order.line.wizard.tree"
 #    name = fields.Char(string='Tarea')
 #    wizard_id = fields.Many2one('sale.order.line.wizard', string='Tarea de linea de venta')
 
-class SaleOrderLineTaskWizard(models.TransientModel):
-    _name = "sale.order.line.wizard"
-    
-    line_id = fields.Many2one('sale.order.line', string='Linea de Venta')
-    #subtask_ids = fields.One2many('sale.order.line.subtask', 'wizard_id', string='Sub Tareas')
-
-    def added(self):
-        pass
+#class SaleOrderLineTaskWizard(models.TransientModel):
+#    _name = "sale.order.line.wizard"
+#    
+#    line_id = fields.Many2one('sale.order.line', string='Linea de Venta')
+#    subtask_ids = fields.One2many('sale.order.line.subtask', 'wizard_id', string='Sub Tareas')
+#
+#    def added(self):
+#        pass
 #        for line in self.subtask_ids:
 #            subtask = self.env['sale.order.line.subtask'].sudo().create({
 #				'name':line.name,
